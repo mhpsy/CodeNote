4a84393d36d9d7054a48f93824325f6d2f22f32b
A pessimistic guide to living <82896623+mhpsy@users.noreply.github.com>
2023-02-20 00:21:02 +0800

    382零钱系统

---
+++ java/1.basic/_01Array/src/main/java/top/mhpsy/changeSys/openSmallChange.java
@@ -0,0 +1,9 @@
+package top.mhpsy.changeSys;
+
+public class openSmallChange {
+    public static void main(String[] args) {
+        smallChangeSysOOP smallChangeSysOOP = new smallChangeSysOOP();
+        System.out.println("Welcome to small change sys oop version");
+        smallChangeSysOOP.menu();
+    }
+}
