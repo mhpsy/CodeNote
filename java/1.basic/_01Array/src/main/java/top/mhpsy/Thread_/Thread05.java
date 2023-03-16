90efa9d0bed104565a1160c31fcb51128883b532
A pessimistic guide to living <82896623+mhpsy@users.noreply.github.com>
2023-03-16 22:32:54 +0800

    java多线程

---
+++ java/1.basic/_01Array/src/main/java/top/mhpsy/Thread_/Thread05.java
@@ -0,0 +1,37 @@
+package top.mhpsy.Thread_;
+
+public class Thread05 {
+    public static void main(String[] args) {
+        Thread t5 = new Thread(new T5(), "T5");
+        t5.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main " + (i + 1));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (i == 5) {
+                try {
+                    t5.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
+
+class T5 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("我是T5");
+        }
+    }
+}
---
+++ java/1.basic/_01Array/src/main/java/top/mhpsy/Thread_/Thread07.java
@@ -0,0 +1,32 @@
+package top.mhpsy.Thread_;
+
+public class Thread07 {
+    public static void main(String[] args) {
+        Thread t7 = new Thread(new T7(), "T7");
+        t7.setDaemon(true);//设置为守护线程
+        t7.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main " + (i + 1));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class T7 implements Runnable {
+    @Override
+    public void run() {
+        for (; ; ) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("我是T7");
+        }
+    }
+}
